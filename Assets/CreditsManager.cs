94d99468b2e10d242720333ddbc1a5886f52b244
Allen Ho <allenho2684@gmail.com>
2025-10-21 00:14:33 -0400

    Cleaned up main menu

--- Assets/CreditsManager.cs
+++ Assets/CreditsManager.cs
@@ -3,15 +3,23 @@ using UnityEngine.SceneManagement;
 public class CreditsManager : MonoBehaviour
 {
     public GameObject creditsPanel;
+    public GameObject creditsButton;
     public void ShowCredits()
     {
         if (creditsPanel != null)
+        {
             creditsPanel.SetActive(true);
+            creditsButton.SetActive(false);
+        }
     }
     public void HideCredits()
     {
         if (creditsPanel != null)
+        {
             creditsPanel.SetActive(false);
+            creditsButton.SetActive(true);
+        }
+            
     }
     public void StartNewGame()
     {
